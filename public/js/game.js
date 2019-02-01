041d6628855fa5e0f593f4be61b4e91f7982f655
SgtStarshine <starshine621@ymail.com>
2019-01-31 21:49:24 -0500

    update with issues

--- public/js/game.js
+++ public/js/game.js
@@ -1,8 +1,16 @@
 // ------------------------ Game Functions are below ------------------------
 $(document).ready(function () {
+
+	
+
 	var url = window.location.search;
 	if (url.indexOf("?charID=") !== -1) {
 		charID = url.split("=")[1];
+	}
+	
+	var currentQuestion;
+	if (url.indexOf("&Q=") !== -1) {
+		currentQuestion = url.split("=")[1];
 	}
 	// variable to store the current character made in main.html
 	var curChar = {
@@ -36,12 +44,37 @@ $(document).ready(function () {
 	});
 
 	// Intialize the game with hidden Divs
-	$("#question1").show();
-	$("#question2").hide();
-	$("#question3").hide();
-	$("#question4").hide();
-	$("#question5").hide();
-	$("#question6").hide();
+
+	switch (curChar.currentQuestion) {
+		case "question2":
+			$("#question1").hide();
+			$("#question2").show();
+			break;
+		case "question3":
+			$("#question2").hide();
+			$("#question3").show();
+			break;
+		case "question4":
+			$("#question3").hide();
+			$("#question4").show();
+			break;
+		case "question5":
+			$("#question4").hide();
+			$("#question5").show();
+			break;
+		case "question6":
+			$("#question5").hide();
+			$("#question6").show();
+			break;
+		default:
+			$("#question1").show();
+			$("#question2").hide();
+			$("#question3").hide();
+			$("#question4").hide();
+			$("#question5").hide();
+			$("#question6").hide();
+	}
+
 
 	function handleQuestionSelection(gameQuestion) {
 
@@ -56,26 +89,23 @@ $(document).ready(function () {
 		//show/hide the correct questions 
 		switch (gameQuestion) {
 			case "question1":
-			$(".stepImage").attr("src", "images/nick_the_lounge_singer.jpg");
-
+				$(".stepImage").attr("src", "images/nick_the_lounge_singer.jpg");
 				if (Q1 == "option1") {
 					curChar.knowlege++;
 					curChar.power--;
-					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power)
-					$("#question1").hide();
-					$("#question2").show();
+					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power, currentQuestion)
+					
+					currentQuestion = "question2";
 				} else if (Q1 == "option2") {
 					curChar.power++;
 					curChar.sanity--;
-					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power)
-					$("#question1").hide();
-					$("#question2").show();
+					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power, currentQuestion)
+					currentQuestion = "question2";
 				} else if (Q1 == "option3") {
 					curChar.sanity++;
 					curChar.knowlege--;
-					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power)
-					$("#question1").hide();
-					$("#question2").show();
+					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power, currentQuestion)
+					currentQuestion = "question2";
 				}
 				break;
 
@@ -87,21 +117,18 @@ $(document).ready(function () {
 				if (Q2 == "option1") {
 					curChar.knowlege++;
 					curChar.power--;
-					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power)
-					$("#question2").hide();
-					$("#question3").show();
+					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power, currentQuestion)
+					currentQuestion = "question3";
 				} else if (Q2 == "option2") {
 					curChar.power++;
 					curChar.sanity--;
-					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power)
-					$("#question2").hide();
-					$("#question3").show();
+					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power, currentQuestion)
+					currentQuestion = "question3";
 				} else if (Q2 == "option3") {
 					curChar.sanity++;
 					curChar.knowlege--;
-					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power)
-					$("#question2").hide();
-					$("#question3").show();
+					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power, currentQuestion)
+					currentQuestion = "question3";
 				}
 
 				break;
@@ -112,21 +139,18 @@ $(document).ready(function () {
 				if (Q3 == "option1") {
 					curChar.knowlege++;
 					curChar.power--;
-					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power)
-					$("#question3").hide();
-					$("#question4").show();
+					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power, currentQuestion)
+					currentQuestion = "question4";
 				} else if (Q3 == "option2") {
 					curChar.power++;
 					curChar.sanity--;
-					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power)
-					$("#question3").hide();
-					$("#question4").show();
+					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power, currentQuestion)
+					currentQuestion = "question4";
 				} else if (Q3 == "option3") {
 					curChar.sanity++;
 					curChar.knowlege--;
-					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power)
-					$("#question3").hide();
-					$("#question4").show();
+					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power, currentQuestion)
+					currentQuestion = "question4";
 				}
 
 				break;
@@ -137,19 +161,19 @@ $(document).ready(function () {
 				if (Q4 == "option1") {
 					curChar.knowlege++;
 					curChar.power--;
-					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power)
+					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power, currentQuestion)
 					$("#question4").hide();
 					$("#question5").show();
 				} else if (Q4 == "option2") {
 					curChar.power++;
 					curChar.sanity--;
-					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power)
+					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power, currentQuestion)
 					$("#question4").hide();
 					$("#question5").show();
 				} else if (Q4 == "option3") {
 					curChar.sanity++;
 					curChar.knowlege--;
-					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power)
+					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power, currentQuestion)
 					$("#question4").hide();
 					$("#question5").show();
 				}
@@ -162,19 +186,19 @@ $(document).ready(function () {
 				if (Q5 == "option1") {
 					curChar.knowlege++;
 					curChar.power--;
-					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power)
+					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power, currentQuestion)
 					$("#question5").hide();
 					$("#question6").show();
 				} else if (Q5 == "option2") {
 					curChar.power++;
 					curChar.sanity--;
-					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power)
+					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power, currentQuestion)
 					$("#question5").hide();
 					$("#question6").show();
 				} else if (Q5 == "option3") {
 					curChar.sanity++;
 					curChar.knowlege--;
-					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power)
+					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power, currentQuestion)
 					$("#question5").hide();
 					$("#question6").show();
 				}
@@ -186,24 +210,24 @@ $(document).ready(function () {
 				if (Q6 == "option1") {
 					curChar.knowlege++;
 					curChar.power--;
-					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power)
+					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power, currentQuestion)
 					$("#question6").hide();
 				} else if (Q1 == "option2") {
 					curChar.power++;
 					curChar.sanity--;
-					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power)
+					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power, currentQuestion)
 					$("#question6").hide();
 				} else if (Q1 == "option3") {
 					curChar.sanity++;
 					curChar.knowlege--;
-					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power)
+					updateCharacter(curChar.id, curChar.knowlege, curChar.sanity, curChar.power, currentQuestion)
 					$("#question6").hide();
 				}
 
 				endGame();
 
 				break;
-				
+
 			default:
 				//this should log an error **TODO**
 				break;
@@ -211,7 +235,7 @@ $(document).ready(function () {
 		return;
 	}
 
-	function updateCharacter(charID, knowlege, sanity, power) {
+	function updateCharacter(charID, knowlege, sanity, power, currentQuestion) {
 
 		var updatedCharacter = {
 			id: charID,
@@ -226,11 +250,12 @@ $(document).ready(function () {
 		updateCharacter.knowlege = knowlege;
 		updateCharacter.sanity = sanity;
 
+		alert(currentQuestion);
 		$.ajax({
 			method: "PUT",
 			url: "/api/updateCharacter",
 			data: updatedCharacter
-		}).then($(location).attr('href',"/game?charID=" + curChar.id));
+		}).then($(location).attr('href', "/game?charID=" + curChar.id + "&Q=" + currentQuestion));
 	}
 
 	function updatePlayerAttributesView() {
@@ -251,7 +276,7 @@ $(document).ready(function () {
 	// Function to be run after the timer is up
 	function endGame() {
 		$(".stepImage").remove();
-		
+
 		if (curChar.power >= 8 && curChar.power > curChar.knowlege && curChar.power > curChar.sanity) {
 			$("#ending1").show();
 			alert("That's a lot of knowledge!!!")
